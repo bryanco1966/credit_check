0ebb5bae6c6bf28abfb389e3eb0fd5b1cc9a91bb
Bryan Combs <34721313+bryanco1966@users.noreply.github.com>
2018-03-17 16:45:55 -0600

    Add files via upload

--- credit_check.rb
+++ credit_check.rb
@@ -1,65 +1,62 @@
-class CreditCard
-
-  attr_reader :cardnumber
-
-  def initialize (cardnumber)
-    @cardnumber = cardnumber
-  end
-
-  def create_integer_array
-    @cardnumber.to_i.digits.drop(1)
-  end
 
 
-  # def remove_first_number
-  #   @cardnumber.drop(1)
-  # end
+def create_integer_array (card)
+       card.to_i.digits
+end
 
-  def double_evens
-    @cardnumber.map.with_index do |number, index|
-      if index % 2 == 0
+
+def double_evens (card)
+    card.map.with_index do |number, index|
+      if index % 2 == 1
         number * 2
       else
         number
       end
     end
-  end
+end
 
-    def modify_over_ten
-      @cardnumber.map do |number|
+
+def modify_over_ten (card)
+      card.map do |number|
         if number >= 10
         number -= 9
         else
         number
         end
       end
-    end
+end
 
-def sum_the_numbers
-  @cardnumber.inject(0) do |sum, n|
+
+def sum_the_numbers (card)
+  card.inject(0) do |sum, n|
      sum + n
    end
 end
 
-def return_valid
-  if @cardnumber % 10 == 0
-    @cardnumber = "The card is valid!"
+
+def return_valid (card)
+  if card % 10 == 0
+    card = "The card is valid!"
   else
-    @cardnumber = "The cards is not valid!"
+    card = "The cards is not valid!"
   end
 end
 
+
+def luhn (card)
+
+  step1 = create_integer_array(card)
+  step2 = double_evens(step1)
+  step3 = modify_over_ten(step2)
+  step4 = sum_the_numbers(step3)
+  step5 = return_valid(step4)
+  puts step5
 end
 
 
-card = CreditCard.new ("492973547750543")
 
-card1 = card.create_integer_array
 
-card1 = card.double_evens
 
-card_modified = card_double_evens.modify_over_ten
-
-card_sum = card_modified.sum_the_numbers
-
-card_valid = card_sum.return_valid
+card = ("342804633855673")
+valid = luhn(card)
+puts valid
